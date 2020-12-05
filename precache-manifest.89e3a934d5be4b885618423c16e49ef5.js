19431f654986cfb0e50ddc58c3dd33ab4e5987ba
muratcankirdar <muratcankirdar@gmail.com>
2020-12-05 15:00:38 +0300

    deploy settings

---
+++ precache-manifest.89e3a934d5be4b885618423c16e49ef5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b15beb0be7289c65ce8",
+    "url": "/vue-quotes/css/chunk-vendors.878890e9.css"
+  },
+  {
+    "revision": "f44c7de685023fdd21e446c659c9d3eb",
+    "url": "/vue-quotes/index.html"
+  },
+  {
+    "revision": "051f4c0dd083d0552264",
+    "url": "/vue-quotes/js/app.f2376e0a.js"
+  },
+  {
+    "revision": "4b15beb0be7289c65ce8",
+    "url": "/vue-quotes/js/chunk-vendors.0bfda75f.js"
+  },
+  {
+    "revision": "9ec0507b7ae69bbb0e0d779e072dadc7",
+    "url": "/vue-quotes/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-quotes/robots.txt"
+  }
+]);
